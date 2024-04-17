73fa1f4b5f75ff264757a8b6bbe333794bc54fda
Samuel Billot <samuel.billot@outlook.fr>
2024-04-18 00:59:51 +0200

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
+function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Router basename={"samuelbillot"}>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    console.log("Hello");
+    
+
+    return (
+        <h1>Home</h1>
+    )
+}
