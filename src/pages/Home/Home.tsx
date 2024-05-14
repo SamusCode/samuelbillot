352c3bae4f409d2508bdba57c587187363e2041f
Samuel Billot <samuel.billot@outlook.fr>
2024-05-15 01:40:07 +0200

    Changed upload to download

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                     <li><a href="https://www.linkedin.com/in/samuel-billot-40355b251/" target="_blank" title="LinkedIn"><img src="samuelbillot/src/images/icons/linkedin.svg" alt="Linkedin" /></a></li>
                     <li><a href="https://github.com/Solarrris" target="_blank" title="GitHub"><img src="samuelbillot/src/images/icons/github.svg" alt="Github" /></a></li>
                 </ul>
-                <a className="resume" href="samuelbillot/src/assets/Resume.pdf" download="BILLOT-Samuel-Resume">Upload my resume</a>
+                <a className="resume" href="samuelbillot/src/assets/Resume.pdf" download="BILLOT-Samuel-Resume">Download my resume</a>
             </div>
         </div>
     )
