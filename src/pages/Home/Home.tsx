27cd9f0aa454fe6a3a1244ea8e34a3f05fe654cc
Samuel Billot <samuel.billot@outlook.fr>
2024-05-27 16:27:51 +0200

    Working on responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import "./Home.scss";
 export default function Home() {
     return (
         <div id="home">
-            <div className="line-1">
-                <svg width="1920" height="359" viewBox="0 0 1920 359" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <div className="lines">
+                <svg className="line" width="1920" height="359" viewBox="0 0 1920 359" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M-60 4C205.216 218.183 390.868 -32.0308 417.39 122.101C443.911 276.233 847.04 404.342 1032.69 336.284C1218.34 268.226 1844.25 52.0411 1952.99 160.134M1952.99 160.134C2039.98 246.608 1996.31 201.502 1963.6 168.14L1952.99 160.134Z" stroke="#65D8FD" strokeWidth="8" />
                 </svg>
                 <svg className="glow" width="1920" height="389" viewBox="0 0 1920 389" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -19,9 +19,7 @@ export default function Home() {
                         </filter>
                     </defs>
                 </svg>
-            </div>
-            <div className="line-2">
-                <svg width="1920" height="359" viewBox="0 0 1920 359" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="line" width="1920" height="359" viewBox="0 0 1920 359" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M2004 355C1740.2 140.817 1555.53 391.031 1529.15 236.899C1502.77 82.7672 1101.79 -45.3424 917.131 22.7158C732.469 90.774 109.894 306.959 1.73483 198.866M1.73483 198.866C-84.7926 112.392 -41.353 157.498 -8.81729 190.86L1.73483 198.866Z" stroke="#FEBB78" strokeWidth="8" />
                 </svg>
                 <svg className="glow" width="1920" height="389" viewBox="0 0 1920 389" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -37,11 +35,15 @@ export default function Home() {
                     </defs>
                 </svg>
             </div>
-            <div className="title">
-                <h1>SAMUEL BILLOT</h1>
+            <div className="content">
+                <div className="mobile">
+                    <h1>SAMUEL</h1>
+                    <h1>BILLOT</h1>
+                </div>
+                <h1 className="main-title">SAMUEL BILLOT</h1>
                 <h2>WEB DEVELOPER</h2>
+                <a href="#projects">Read More</a>
             </div>
-            <a href="#projects">Read More</a>
         </div>
     )
 }
